13b5172da20ffe3ed488a42a2e123078ee576dc6
Allan-Victor <akibuuka50@gmail.com>
2017-07-03 06:58:31 -0700

    Create Prime.py

---
+++ Prime.py
@@ -0,0 +1,8 @@
+def num_is_Prime(value):
+    for num in range(2,value):
+        if value%num==0:
+             return false
+        return true      
+
+[num for num in range(1,100)] 
+if num_is_Prime(num)  
